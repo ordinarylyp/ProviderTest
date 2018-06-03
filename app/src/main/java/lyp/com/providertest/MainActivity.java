f42214b47eeecfa348bf0bd8269653b54aa7417f
ordinarylyp <2628179064@qq.com>
2018-06-03 16:13:26 +0800

    Initial commit

---
+++ app/src/main/java/lyp/com/providertest/MainActivity.java
@@ -0,0 +1,80 @@
+package lyp.com.providertest;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String newId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button addData=findViewById(R.id.add_data);
+        addData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("content://lyp.com.databasetest.provider/book");
+                ContentValues values=new ContentValues();
+                values.put("name","第一行代码");
+                values.put("author","郭霖");
+                values.put("pages",570);
+                values.put("price",70);
+                Uri newUri=getContentResolver().insert(uri,values);
+                newId=newUri.getPathSegments().get(1);
+            }
+        });
+
+        Button queryData=findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("content://lyp.com.databasetest.provider/book");
+                Cursor cursor=getContentResolver().query(uri,null,null,null,
+                        null);
+                if (cursor!=null){
+                    while (cursor.moveToNext()){
+                        String name=cursor.getString(cursor.getColumnIndex("name"));
+                        String author=cursor.getString(cursor.getColumnIndex("author"));
+                        int pages=cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price=cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is "+name);
+                        Log.d("MainActivity","book author is "+author);
+                        Log.d("MainActivity","book pages is "+pages);
+                        Log.d("MainActivity","book price is "+price);
+                    }
+                    cursor.close();
+                }
+            }
+        });
+
+        Button updateData=findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("content://lyp.com.databasetest.provider/book");
+                ContentValues values=new ContentValues();
+                values.put("name","第二行代码");
+                values.put("pages",682);
+                values.put("price",87.5);
+                getContentResolver().update(uri,values,null,null);
+            }
+        });
+
+        Button deleteData=findViewById(R.id.delete_data);
+        deleteData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri=Uri.parse("content://lyp.com.databasetest.provider/book"+newId);
+                getContentResolver().delete(uri,null,null);
+            }
+        });
+    }
+}
